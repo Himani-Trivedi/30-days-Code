a751886afe512ffd4096e30f3192eed5b6744a54
Himani-Trivedi <himanitrivedi1874@gmail.com>
2023-03-06 20:33:45 +0530

    14 done

--- Day-14/day14_himani.py
+++ Day-14/day14_himani.py
@@ -1,7 +1,34 @@
 
-# countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
-# names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+from re import A
+
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 # 1. Use a map to create a new list by changing each country and name to uppercase in the countries list
+countries=list(map(lambda s: s.upper(), countries))
+names=list(map(lambda x : x.upper(),names))
+print(countries)
+print(names)
+
 # 2. Use a filter to filter out countries having precisely six characters.
+newCountries=list(filter(lambda x:len(x) == 6,countries))
+print(newCountries)
+
 # 3. Using the generator print the Fibonacci series.
+
+
+def fibo(n):
+    a=0
+    b=1
+
+    while True:
+        yield a
+        t=a+b
+        a=b
+        b=t
+
+x=fibo(6)
+for i in range(7):
+    print(next(x))
