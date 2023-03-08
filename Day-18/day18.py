d9190a617afba25772462a8da7e0fe261e128bf5
Himani-Trivedi <himanitrivedi1874@gmail.com>
2023-03-08 19:42:05 +0530

    regular expression

--- Day-18/day18.py
+++ Day-18/day18.py
@@ -1,13 +1,37 @@
-# Day 18: In the GitHub repository, Infinity-AI(Infinite Learn and Grow)/30-days-code inside the Day-18 folder create your_name.py file as defined in the readme.md file.
+# Day 18: In the GitHub repository, Infinity-AI(Infinite Learn and Grow)/30-days-code inside the Day-18 folder create your_name.py 
+# file as defined in the readme.md file.
+
 # 1. Write a pattern which identifies if a string is a valid python variable
-# is_valid_variable('first_name') # True
-# is_valid_variable('first-name') # False
-# is_valid_variable('1first_name') # False
-# is_valid_variable('firstname') # True
+
+
+
+import re
+
+
+def is_valid_variable(name):
+    pattern=re.compile("^([a-z]|[A-Z]|_)([a-z]|[A-Z]|[0-9]|_)*$")
+    match=pattern.match(name)
+    if match: 
+        print(True)
+    else:
+        print(False)
+
+is_valid_variable('first_name') # True
+is_valid_variable('first-name') # False
+is_valid_variable('1first_name') # False
+is_valid_variable('first 89name') # False
+is_valid_variable('firstname') # True
 
 # 2. Clean the following text. After cleaning, count three most frequent words in the string.
-# sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
 
-# print(clean_text(sentence));
+def clean_text(sen):
+    repalce=''
+    patten=re.compile(r"[^a-zA-Z\s]")
+    new_sen=re.sub(patten,repalce,sen)
+    return new_sen
+
+print(clean_text(sentence));
+
 # I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher
 # print(most_frequent_words(cleaned_text)) # [(3, 'I'), (2, 'teaching'), (2, 'teacher')]
---
+++ Day-20/day20.py
@@ -0,0 +1,5 @@
+# 1. Read this url and find the 10 most frequent words. romeo_and_juliet = 'http://www.gutenberg.org/files/1112/1112.txt'
+# 2. Read the countries API and find
+#    a. the 10 largest countries
+#    b. the 10 most spoken languages
+#    c. the total number of languages in the country's API
